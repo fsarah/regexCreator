f8b0623b090bb8e1518795cb6273b6b488f29337
fsarah <sarah.frank@student.tugraz.at>
2016-10-04 22:40:02 +0200

    Add files via upload
    some small changes

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def simplify(input_summary):
     return simplified_summary
 
 def vagueify(input_summary):
-    print input_summary
+    print (input_summary)
     for line in input_summary:
         if len(input_summary[line]) > 1:
             numbers = 0
@@ -58,11 +58,36 @@ def vagueify(input_summary):
                 if char.isalpha():
                     chars += 1
             if numbers > 1 and chars == 0:
-                input_summary[line] = "(0-9)"
+                input_summary[line] = []
+                input_summary[line].append("(0-9)")
             elif numbers > 1 and chars > 1:
-                input_summary[line] = "(0-9)|(a-z)"
+                input_summary[line] = []
+                input_summary[line].append("(0-9)|(a-z)")
+            elif numbers > 1 and chars == 1:
+                input_summary[line] = []
+                input_summary[line].append("(0-9)")
+                input_summary[line].append("char") #TODO
+            elif numbers == 1 and chars > 1:
+                input_summary[line] = []
+                input_summary[line].append("(a-z)")
+                input_summary[line].append("number") #todo
             elif numbers == 0 and chars > 0:
-                input_summary[line] = "(a-z)"
+                input_summary[line] = []
+                input_summary[line].append("(a-z)")
+    return input_summary
+
+def summarize(vague_summary):
+  print(vague_summary)
+  for line in vague_summary:
+    if line > 0 and vague_summary[line] == vague_summary[line - 1]:
+      vague_summary[line] = []
+#      if vague_summary[line - 1] DOESN'T HAVE A STAR AT THE END
+      #temp_string = vague_summary[line - 1]
+      #temp_string = temp_string + "*"
+      #vague_summary[line - 1] = temp_string
+      print(vague_summary[line - 1])
+
+
 
 def create_regex(input_summary, shortest_input_length, longest_input_length):
     regex = ""
@@ -75,10 +100,12 @@ def create_regex(input_summary, shortest_input_length, longest_input_length):
         if len(elements) == 1:
             if input_len <= shortest_input_len:
                 regex = regex + elements[0]
+            #else if regex is already in brackets
             else:
                 regex = regex + "(" + elements[0] + ")?"
         elif len(elements) > 1:
             tmp = ""
+
             for element in elements:
                 if tmp == "":
                     tmp = tmp + element
@@ -88,7 +115,6 @@ def create_regex(input_summary, shortest_input_length, longest_input_length):
                 regex = regex + "(" + tmp + ")"
             else:
                 regex = regex + "(" + tmp + ")?"
-
     return regex
 
 
@@ -121,6 +147,7 @@ input_summary = simplify(input_summary)
 #    print input_summary[line]
 
 vague_summary = vagueify(input_summary)
+summarized_summary = summarize(vague_summary)
 
 exact_regex = create_regex(input_summary, shortest_input_len, longest_input_len)
-print exact_regex
+print (exact_regex)
