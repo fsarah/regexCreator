fba3388a8603a43d526f1666c6c3942290dbba04
Sarah Frank <sarah.frank@student.tugraz.at>
2017-02-13 14:49:31 +0100

    check solution finished

--- checksolution.py
+++ checksolution.py
@@ -4,14 +4,16 @@ matches = []
 nonmatches = []
 
 f = open('test_long.txt', 'r')
-rex = re.compile(r"(0-9)((0-9)|(a-z))*(0-9)*") # enter regex here
+rex = re.compile(r"[0-9][[0-9]|[a-z]]*[0-9]*") # enter regex here
 
 for line in f:
     newline = line.rstrip()
-    if rex.match(newline):
-        matches.append(newline)
-    else:
-        nonmatches.append(newline)
+
+    if len(newline) > 0:
+        if rex.match(newline):
+            matches.append(newline)
+        else:
+            nonmatches.append(newline)
 
 print("Matches: ", matches)
 print("NonMatches: ", nonmatches)
